f76182f1f1b8437258c6380fb25afcaa5b92f646
Winniebob <vilksmax@gmail.com>
2023-05-23 17:28:32 +0300

    Программа которая принимает на входе два числа и проверяет, является ли одно из них квадартом другого

---
+++ Task16/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+Console.WriteLine("Введите число 1");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число 2");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+bool result = Multiplicity(number1,number2);
+
+Console.WriteLine (result ? $"да" : "нет");
+
+bool Multiplicity(int num1,int num2)
+{
+    return num1==num2*num2 || num2==num1*num1;
+}
