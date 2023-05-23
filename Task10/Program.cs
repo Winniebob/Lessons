69da4da4cc03818c6633c8bbe2d127f8047143a1
Winniebob <vilksmax@gmail.com>
2023-05-23 17:55:02 +0300

    Программа принимающая на входе три цифры и отдающая вторую

---
+++ Task10/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу которая принимает на вход трехзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int secondNumber = number / 10 % 10;
+Console.WriteLine($"Введенное число {number} ");
+Console.WriteLine($"Вторая цифра {secondNumber} ");
+
+
+    if (number<=100&&number>=1000)
+    {
+        Console.WriteLine($"Некорректный ввод");
+    }
+    return;
