465748afeaae1337ffc0169fa0537135a2a8039f
Winniebob <vilksmax@gmail.com>
2023-05-23 17:16:56 +0300

    Программа для проверки кратности одновременно к двум числам

---
+++ Task14/Program.cs
@@ -0,0 +1,13 @@
+﻿// метод нахождения кратных чисел, проверяющий кратность одновременно к 7 и 23
+Console.WriteLine("Введите число ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool result = Multiplicity(number); 
+
+Console.WriteLine (result ? $"да" : "нет");
+bool Multiplicity(int num)
+{ 
+  return  num % 7 == 0 && num % 23==0;
+
+}
