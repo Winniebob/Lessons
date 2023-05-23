3f61a5c6ddc9585b1cd0ba545bf344e0e9cb7254
Winniebob <vilksmax@gmail.com>
2023-05-23 16:17:37 +0300

    Программа поиска меньшего из большего числа

---
+++ Task02/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу которая на вход принимает два числа и выает какое число больше, а какое меньше. a=5 b =7 -> max=7
+
+
+Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if ( number1 > number2 )
+{   
+Console.WriteLine($"большее число это {number1} меньшее {number2}");
+}
+else
+{
+Console.WriteLine($"большее число это {number2} меньшее {number1}");
+}
