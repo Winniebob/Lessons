062669e95537b5eb87e62e17c53936d1240f26ee
Winniebob <vilksmax@gmail.com>
2023-05-23 16:18:41 +0300

    Где из числа N, показывают все числа от -N до N

---
+++ Task05/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу которая на вход принимает одно число (N) а на выходе показывает все целые числа от -N до N
+
+Console.WriteLine("Введите целое положительное число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int count = -number1; // где -number это число счетчика который мы применяем 
+
+while ( count <= number1 )
+{
+   Console.Write ($"{count} ");
+   count++;
+}
