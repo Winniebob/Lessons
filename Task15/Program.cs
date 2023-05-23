e9450906613febd999256c1af0d4192b4f0ae355
Winniebob <vilksmax@gmail.com>
2023-05-23 20:39:03 +0300

    Напишите программу которая принимает на вход цифру, обозначающую день недели и проверяет явялется ли этот день выходным

---
+++ Task15/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine("Введите день недели");
+int weekDay = Convert.ToInt32(Console.ReadLine());
+
+if (ValldateWeekend(weekDay))
+{   
+    if (IsWeekend(weekDay))
+       {
+    Console.WriteLine("Выходной!");
+       }
+    else 
+    {
+    Console.WriteLine("Arbaiten match frai!!"); 
+    }
+}
+
+
+
+bool IsWeekend(int weekDay)
+{
+    if (weekDay>5)
+    {
+       return true;
+    }
+   return false;
+}
+
+bool ValldateWeekend (int weekDay)
+{
+    if ( weekDay >0 && weekDay<=7)
+    {
+       return true;
+    }
+    
+        Console.WriteLine("Это не день недели!");
+        return false;
+
+}
+
