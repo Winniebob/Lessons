8bc634fc857e9648a8ea43c8177ce861178b5844
Winniebob <vilksmax@gmail.com>
2023-05-23 16:19:45 +0300

    Четные от одного до N

---
+++ Task08/Program.cs
@@ -0,0 +1,15 @@
+﻿// показать четные числа от 1 до N
+
+Console.WriteLine("Введите  число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int evenNumber = 2;
+
+if(number > 1)
+{
+    while(evenNumber <= number)
+    {
+        Console.Write(evenNumber + " ");
+        evenNumber = evenNumber + 2;
+    }
+}
