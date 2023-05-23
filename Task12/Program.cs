ec762c8e046f325ef1c3f91acf34fd5bd1114b06
Winniebob <vilksmax@gmail.com>
2023-05-23 16:56:31 +0300

    Нахождение четного и нечетного числа, вывод остатка

--- Task12/Program.cs
+++ Task12/Program.cs
@@ -5,12 +5,15 @@ int number1 = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Введите число 2");
 int number2 = Convert.ToInt32(Console.ReadLine());
-int remainder = number1 % number2;
-if ( remainder == 0)
+
+String result = remained != 0 ? "кратное" : $" Не кратное = {Remained}";
+
+Console.WriteLine(result);
+
+int remained = Remained(number1,number2);
+
+int Remained (int num1, num2)
 {
-    Console.WriteLine("кратное");
-}
-else 
-{
-    Console.WriteLine($" Не кратное = {remainder}");
+    int remained = num1 % num2;
+    return  remained;
 }
