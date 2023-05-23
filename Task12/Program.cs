0b79ba21391a33641bb684872ddb8f31ad79f240
Winniebob <vilksmax@gmail.com>
2023-05-23 16:45:13 +0300

    Нахождение четного и нечетного числа, вывод остатка

---
+++ Task12/Program.cs
@@ -0,0 +1,16 @@
+﻿// Нахождение четного не четного числа. При нахождении не четного вывод остатка.
+
+Console.WriteLine("Введите число 1");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число 2");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int remainder = number1 % number2;
+if ( remainder == 0)
+{
+    Console.WriteLine("Четное");
+}
+else 
+{
+    Console.WriteLine($" Не четное = {remainder}");
+}
