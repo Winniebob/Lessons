24cc20c487a0e8a46d93f20d6151e141f4d88882
Winniebob <vilksmax@gmail.com>
2023-05-24 14:35:27 +0300

    Напишите программу которая по заданному номеру четверти показывает диапазон возможных координат точек в этой четверти (x и y)

---
+++ Task18/Program.cs
@@ -0,0 +1,36 @@
+﻿// Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти ( х и у).
+
+Console.WriteLine("Введите номер четверти ");
+int quarter = Convert.ToInt32(Console.ReadLine());
+
+int range = Range(quarter);
+
+string result = range>0 ? $"Указанные номер четверти соотвтествуют диапазону -> {quarter}" : "Введены некорректные координаты";
+
+Console.WriteLine(result);
+
+int Range (int quarter)
+{
+    if (quarter ==1) 
+    {
+    Console.WriteLine("Диапозон от +X до +Y ");
+        return 1;
+    }
+
+    if (quarter ==2) 
+    {
+        Console.WriteLine("Диапозон от -X до +Y ");
+        return 2;
+    }
+    if (quarter ==3) 
+    {
+        Console.WriteLine("Диапозон от -X до -Y ");
+        return 3;
+    }
+    if (quarter ==4) 
+    {
+        Console.WriteLine("Диапозон от +X до -Y ");
+        return 4;
+    }
+    return 0;
+}
