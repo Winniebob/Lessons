59f0b57579ba0ed1af20da5c1626e0da58b95260
Winniebob <vilksmax@gmail.com>
2023-05-24 16:02:14 +0300

    Напишите программу которая принимает на вход пятизначное число и проверяет является ли оно палиндромом

---
+++ Task19/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу которая рпинимает на вход пятизначное число и проверяет является ли оно палиндромом
+
+Console.WriteLine ("введите пятизначное число");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+Polindrom(numberN);
+
+void Polindrom (int num)
+{
+    if(num>=10000 & num<=100000)
+    {
+    int num1 = num/10000;
+    Console.WriteLine ($"{num1}");
+    int num2 = num/1000%10;
+    Console.WriteLine ($"{num2}");
+    int num3 = num/10%10;
+    Console.WriteLine ($"{num3}");
+    int num4 = num%10;
+    Console.WriteLine ($"{num4}");
+    if (num1==num4 & num2==num3)
+    {
+        Console.WriteLine ($"{num} -> Да");
+    }
+    else
+    {
+        Console.WriteLine ($"{num} -> Нет");
+    }
+    }
+    else
+    {
+        Console.WriteLine(" Число не входит в диапозон");
+    }
+}
