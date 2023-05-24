5b2b0225d3dac05a728eeeb639e6dad523fb8eab
Winniebob <vilksmax@gmail.com>
2023-05-24 17:17:51 +0300

    Напишите программу, которая принмает на входе число и выдает количестов цифр в числе

---
+++ Task26/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу которая принмает на вход число и выдает количество цифр в числе.
+
+Console.WriteLine ("введите число");
+int number= Convert.ToInt32(Console.ReadLine());
+
+int quantiy = QuantityNum(number);
+
+Console.WriteLine($"количество цифр в  {number} - > {quantiy}");
+
+int QuantityNum(int num)
+{
+    int i =0;
+    while (num!=0)
+    {
+        num = num/10;
+        i++;
+    }
+    return i;
+}
