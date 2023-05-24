3920d2e9066ad9f15233ce8cbd1b06f329ec48f1
Winniebob <vilksmax@gmail.com>
2023-05-24 14:16:04 +0300

    Программа которая принмает на вход координаты точки , причем X and Y !=0 и выдает номер четверти плоскотси в которой она находится

---
+++ Task17/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу которая принимает на вход координаты точки (X и Y ) причем X!= 0 Y!=0 и выдает номер четверти плоскости в которой она находится
+
+Console.WriteLine("Введите координаты X ");
+int xCooridnate = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты Y ");
+int yCoordinate = Convert.ToInt32(Console.ReadLine());
+
+
+int quarter = Quater(xCooridnate,yCoordinate ); // отработав метод возвращает  в пременную int  quarter
+
+string result = quarter >0 ? $"Указанные координаты соответствуют четверти -> {quarter}" : "Введены некорректные координаты";
+
+Console.WriteLine(result);
+
+int Quater (int x, int y )
+{
+    
+    if ( x>0 && y>0) return 1;
+    if ( x<0 && y>0) return 2;
+    if ( x<0 && y<0) return 3;  // Else не добавляется потому что, мы идем по циклу вниз и доходя до нужного нам if, выходим из метода с помощью return;
+    if ( x>0 && y<0) return 4;
+    
+    return 0;
+}
