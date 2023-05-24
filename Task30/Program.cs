ce6d85ed0ed0d1e957d1d6e93f5a1c3d399bdbe1
Winniebob <vilksmax@gmail.com>
2023-05-24 17:57:04 +0300

    Напишите программу, которая выводит массив из 8 элементов заполненный нулями единицами в случайном порядке

---
+++ Task30/Program.cs
@@ -0,0 +1,29 @@
+﻿ // Напишите прогармму которая выводит массив из 8 элементов зхаполненный нулями и единицами в слчамйном порядке
+
+ int[] arr = new int [8];
+
+ void PrintArray(int[]array) 
+ {  
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+  
+ }
+
+ void FillArray(int[] array)
+ {
+    var rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        array[i] = rnd.Next(0,2);
+    }
+
+ }
+
+FillArray(arr);
+Console.Write("[");
+ 
+ 
+ PrintArray(arr);
+ Console.Write("]");
