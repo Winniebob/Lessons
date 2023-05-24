5e950667ea6e6f1a9e74d6d4860dbb427339ee8e
Winniebob <vilksmax@gmail.com>
2023-05-24 16:32:05 +0300

    Напишите программу, которая принмает на входе число N и выдает таблицу кубов чисел от 1 до N

---
+++ Task23/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принмает на вход число (N) и выдает таблицу кубов числе от 1 до N
+
+
+Console.WriteLine ("введите число");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+Cubos(numberN);
+
+void Cubos(int num)
+{
+    for (int i = 1; i <= num; i++)
+    {
+        int cubos = i*i*i;
+        Console.WriteLine ($"{i,3} {cubos,5} ");
+    }
+        
+}
