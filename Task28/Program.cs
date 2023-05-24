efb2db6ea2d6b79eb951059ed5980ae7220f8aad
Winniebob <vilksmax@gmail.com>
2023-05-24 17:25:45 +0300

    Напишите программу, которая принимает на вход число N и выдает произведение чисел от 1 до N

---
+++ Task28/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу которая принмает на вход число N и выдает произведение чисел от 1 до N
+
+Console.WriteLine ("введите число");
+int number= Convert.ToInt32(Console.ReadLine());
+
+int workNumber = WorkNubmer(number);
+
+Console.WriteLine ($"произведение чисел от 1 до {number} -> {workNumber}");
+
+int WorkNubmer(int num)
+{
+    int work = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        work = work*i;
+    }
+    return work;
+}
