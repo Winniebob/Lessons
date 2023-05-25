4b764d48449b94bee89062a2ca2d1205a3425f3f
Winniebob <vilksmax@gmail.com>
2023-05-25 21:17:38 +0300

    Напишите программу которая будет преобразовывать десятичное число в двоичное

---
+++ Task42/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 42: Напишите программу, которая будет преобразовывать
+//десятичное число в двоичное.
+//46 -> 101110
+//13 -> 1101
+//2 -> 10
+
+string TheCalculusSystem (int num)
+{
+    string count = "";
+    while (num > 0)
+{
+   count = (num % 2) + counter;
+   num = num / 2;
+}
+    return counter;
+}
+
+
+Console.WriteLine("Введите число в двоичной системе исчисления: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string binary = TheCalculusSystem(number);
+Console.WriteLine($"{number} -> {binary}");
