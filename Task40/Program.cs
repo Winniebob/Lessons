b0d31802803bfbf2f6fc82f29a06d183316a76b2
Winniebob <vilksmax@gmail.com>
2023-05-25 21:16:25 +0300

    Напишите программу которая принимает на вход три числа и говорит есть треугольник или нет

---
+++ Task40/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите Координаты точка точек ");
+
+Console.WriteLine("x = ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine("y = ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Z = ");
+int z = Convert.ToInt32(Console.ReadLine());
+
+bool result = RuzTriangle(x,y,z);
+
+Console.WriteLine (result ? $"Да это треугольник!" : "Это не треугольник");
+
+bool RuzTriangle (int x, int y, int z)
+{
+    if( x < y + z & y < x + z & z < y + x )
+    {
+       return true;
+    }
+    return false;
+  
+}
