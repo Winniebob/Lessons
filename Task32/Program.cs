49f7fc292c9e686a89e650992be63eb15133a500
Winniebob <vilksmax@gmail.com>
2023-05-25 14:13:55 +0300

    Напишите программу замены элементов массива: положительные элементы замените на соответсвующие отрицательные и наоборот.

---
+++ Task32/Program.cs
@@ -0,0 +1,41 @@
+﻿// Напишите программу замена элементов массива: положительные элементы замените на соответсвующие отрицательные и наоборот.
+
+  void PrintArray(int[]arr, string sep) 
+ {  
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i<arr.Length - 1) Console.Write($"{arr[i]}{sep} ");
+        else  Console.Write($"{arr[i]} ");
+    }
+  
+ }
+
+ int [] CreateArrayRndInt(int size, int min, int max)
+{
+    int [] arr = new int [size];
+    Random rnd = new Random ();
+
+    for (int i=0; i < arr.Length; i++)
+    {
+
+        arr[i] = rnd.Next(min, max+1);
+    }
+    return arr;
+}
+
+void InverseArray (int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] *= -1;
+    }
+}
+
+
+
+ int[] array = CreateArrayRndInt (12,-9,9);
+ Console.Write("[");
+ PrintArray(array, ",");
+Console.WriteLine("]");
+
+InverseArray(array);
