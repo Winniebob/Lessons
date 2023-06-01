87f156a7f4fa38e57b22ff4875a29e3b8a0d9fec
Winniebob <vilksmax@gmail.com>
2023-06-01 21:20:44 +0300

    Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцыю В случае, если это не возможно, программа должна вывести сообщение для пользователя

---
+++ Task53/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задайте двумерный массив. Напшите программу которая поменяет местами первую и последнюю строку массива.
+
+Console.WriteLine("введите количество строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите количество столбцов");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+
+
+int[,] array2d = CreatMatrixInt(rows,columns);
+Console.WriteLine();
+PrintMatrix(array2d);
+SwitchArrayFirstLastRows(array2d);
+Console.WriteLine();
+PrintMatrix(array2d);
+
+
+int [,] CreatMatrixInt(int rows, int columns)
+{
+    int [,] matrix = new int [rows,columns];
+    
+
+    for (int i=0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+             matrix[i, j] = i + j;
+        }
+
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],5} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SwitchArrayFirstLastRows (int[,] matrix)
+{
+    
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {   
+        int tmp = matrix[0,j];
+        matrix[0,j] = matrix [matrix.GetLength(1)-1,j];
+        matrix [matrix.GetLength(1)-1,j] = tmp;
+    }
+
+}
