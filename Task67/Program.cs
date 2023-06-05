3ce2598f2f19478c8925cd32072b8593438f253e
Winniebob <vilksmax@gmail.com>
2023-06-05 22:00:10 +0300

    Напишите программу которая будет принимать на вход число и возвращать его сумму

---
+++ Task67/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и
+//возвращать сумму его цифр.
+//453 -> 12
+//45 -> 9
+
+Console.WriteLine("введите натуральное число");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(FactorialSum(n));
+
+int FactorialSum(int n)
+ {
+     if (n == 0) return 0;
+     else return n % 10 + FactorialSum(n/10);
+ }
