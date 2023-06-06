b30b85148e9db98ba3b9b1439a56f9010fc2c841
Winniebob <vilksmax@gmail.com>
2023-06-06 18:44:38 +0300

    Задайте значения M и N. Напишите программу, которая найдет сумму натуральных элементов в промежутке от M до N

---
+++ Task66/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N
+
+
+Console.Write("Введите натуральное число M: ");
+double numberM = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите натуральное число N: ");
+double numberN = Convert.ToDouble(Console.ReadLine());
+
+if (numberN < 0 || numberN % 1 != 0)
+{
+
+    Console.Write("Это не натуральное число");
+    return;
+}
+
+if (numberM < 0 || numberM % 1 != 0)
+{
+
+    Console.Write("Это не натуральное число");
+    return;
+}
+
+double sumNaturalNumb = SumNaturalNumb(numberN,numberM);
+Console.WriteLine($"{sumNaturalNumb}");
+
+double SumNaturalNumb (double numN, double numM)
+{
+    
+    if (numN == numM) return numN;
+    else if (numN>numM) return  numN + SumNaturalNumb(numN - 1, numM);
+    else return numM + SumNaturalNumb(numM - 1, numN);
+}
+
