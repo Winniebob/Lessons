a4aca0490f2fac095755319999fa3231905f2468
Winniebob <vilksmax@gmail.com>
2023-06-06 18:43:41 +0300

    задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии

---
+++ Task64/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+
+Console.Write("Введите натуральное число N: ");
+double numberN = Convert.ToDouble(Console.ReadLine());
+
+if (numberN < 0 || numberN % 1 != 0)
+{
+
+    Console.Write("Это не натуральное число");
+    return;
+}
+
+NaturaleNumbers(numberN);
+
+void NaturaleNumbers(double num)
+{
+    if (num == 0) return;
+    Console.Write($"{num} ");
+    NaturaleNumbers(num - 1);
+}
